912bc2080f35025fc2336028b977162f9ae97f34
MartiVilas <marti.vilas14@gmail.com>
2023-12-27 12:25:36 +0100

    Second commit: Añadimos el lectorDeNums.kt

---
+++ src/main/kotlin/lectorDeNums.kt
@@ -0,0 +1,58 @@
+package org.example
+import java.util.*
+fun llegirInt():Int {
+    var scanner=Scanner(System.`in`)
+    var num:Int=0
+    var error = true
+    do {if (scanner.hasNextInt()){
+        num=scanner.nextInt()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+
+fun llegirDouble():Double {
+    var scanner=Scanner(System.`in`)
+    var num:Double=0.0
+    var error = true
+    do {if (scanner.hasNextDouble()){
+        num=scanner.nextDouble()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+fun llegirFloat():Float {
+    var scanner=Scanner(System.`in`)
+    var num:Float = 0.0f
+    var error = true
+    do {if (scanner.hasNextFloat()){
+        num=scanner.nextFloat()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+fun llegirLong():Long {
+    var scanner=Scanner(System.`in`)
+    var num:Long=0
+    var error = true
+    do {if (scanner.hasNextLong()){
+        num=scanner.nextLong()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
